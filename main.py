895023e2c5d19f719e7402f5971b355cece5193d
Matheus Rodrigues De Jesus <matheus.rj25@hotmail.com>
2025-07-24 20:31:54 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from manim import Scene, Circle, Square, Create,Transform
+
+class Animacao(Scene):
+    def construct(self):
+        circulo = Circle() # Círculo
+        quadrado = Square() # Quadrado
+
+        self.play(Create(circulo)) # Cria o círculo na cena
+        self.play(Transform(circulo, quadrado)) # Transforma o círculo em quadrado
+
