e62ab1e02b6096354822fc2d24ee7d45b9e2e844
Matheus Rodrigues De Jesus <matheus.rj25@hotmail.com>
2025-07-28 20:26:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ class Animacao(Scene):
         quadrado = Square() # Quadrado
 
         self.play(Create(circulo)) # Cria o círculo na cena
-        self.pause(3)
+        self.pause(3) # Pausa por 3 segundos
         self.play(Transform(circulo, quadrado)) # Transforma o círculo em quadrado
