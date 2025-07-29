a77770b78028383f9be2ed1a73d7965266de4374
Matheus Rodrigues De Jesus <matheus.rj25@hotmail.com>
2025-07-29 18:08:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # Parei em 4:00 minutos
 from manim import Scene, Circle, Square, Create,Transform
 
+# Cria uma classe de cena que herda de Scene
 class Animacao(Scene):
     def construct(self):
-        circulo = Circle() # Círculo
-        quadrado = Square() # Quadrado
+        circulo = Circle() # Cria um círculo
+        quadrado = Square() # Cria um quadrado
 
         self.play(Create(circulo)) # Cria o círculo na cena
         self.pause(3) # Pausa por 3 segundos
