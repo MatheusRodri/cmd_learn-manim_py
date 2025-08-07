fd935c573b9fa8bb252489a38ae230d168b271ab
Matheus Rodrigues De Jesus <matheus.rj25@hotmail.com>
2025-08-06 22:57:03 -0300

    Update main_2.py

--- main_2.py
+++ main_2.py
@@ -1,23 +1,24 @@
-# Parei em 4:00 minutos
+# Parei em 45:00 minutos
 from manim import Scene # Import de Funções que serão usadas
 from manim import Text,Create, Rectangle, Uncreate # Import de Funções que serão usadas
+from manim import ORANGE
 
 class Animacao(Scene): # Cria uma classe de cena que herda de Scene
     def construct(self):
-        t = Text("Olá, Manim!",color='#40126f') # Cria um texto
-        t2 = Text("Fala Matheus !", color='#000') # Cria um segundo texto
-        r = Rectangle(color='#40126f') # Cria um retângulo
+        t = Text("Olá, Manim!",color='#fff') # Cria um texto
+        t2 = Text("Fala Matheus !", color='#fff') # Cria um segundo texto
+        r = Rectangle(color=ORANGE).scale(2) # Cria um retângulo
 
-        self.camera.background_color = '#ff8972' # Define a cor de fundo da cena
+        self.camera.background_color = '#000' # Define a cor de fundo da cena
         self.play(Create(t)) # Cria o texto na cena
-        self.wait()
+        self.wait(1)
         self.play(Create(r))
 
         self.play(
             Uncreate(t),
-            Create(t),
+            Create(t2),
         )
-        self.wait()
+        self.wait(1)
         self.play(
             Uncreate(t2),
             Uncreate(r)
